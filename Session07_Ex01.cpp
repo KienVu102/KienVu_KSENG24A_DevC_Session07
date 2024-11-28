898ddb18ba82962e49ef17d04489c686a066425b
KienVu102 <vudinhkien1002@gmail.com>
2024-11-28 12:00:48 +0700

    Kiên Vũ upload Session07_Ex01

---
+++ Session07_Ex01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int n1, n2, n3, n4, n5, a[100];
+	printf("Hay nhap lan luot 5 phan tu: ");
+	scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &n5);
+	for(int i=0; i<n; i++){
+		printf("Cac phan tu vua nhap co mang la: %d\n", n);
+	}
+	return 0;
+} 
